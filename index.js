e64d1408096711f6a2faa27887052845f2c489b9
souvik3069 <nathsouvik789@gmail.com>
2025-11-07 12:26:19 +0530

    done

---
+++ index.js
@@ -0,0 +1,12 @@
+/*console.log("Name: Souvik Nath. Role: Full-stack developer. Location: Kolkata. Interests: JavaScript, Node.js, photography. Passionate about building web applications and learning new technologies.");
+var n1 = 10;
+var n2 = 20;
+console.log("Sum of two numbers : " + (n1 + n2));
+
+var n3 = 10;
+var n4 = 5;
+console.log("Subtract of two numbers : " + (n3 - n4));*/
+
+
+
+
